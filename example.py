5175adc0ba6ea335795a3125efa139c5dcb04f99
thaild <thaild@rikkeisoft.com>
2025-11-24 11:10:04 +0700

    feat: add streamlit_dateinput_intl sample

---
+++ example.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from datetime import date, timedelta
+from streamlit_dateinput_intl import streamlit_dateinput_intl
+
+
+st.write("Streamlit dateinput テスト")
+
+selected_date = streamlit_dateinput_intl(
+   value="today",
+   min=date.today() - timedelta(days=30),
+   max=date.today() + timedelta(days=30),
+   key="date_input_full",
+   format="YYYY/MM/DD",
+   disabled=False,
+   width="stretch",
+   locale="ja"
+)
+
+st.write("selected_date type: ", type(selected_date))
+st.write("selected_date: ", selected_date.strftime("%Y/%m/%d"))
